4ee60058e0b1255c9e1766e16f20f231e6a67700
Aso Orji <42071241+asoorji@users.noreply.github.com>
2023-04-07 04:08:02 +0000

    Web statics

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,27 +1,23 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-"""
+""" script that starts a Flask web application:
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes: /: display “Hello HBNB!”
+            /hbnb: display “HBNB”
+    You must use the option strict_slashes=False in your route definition """
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
-    return "Hello HBNB!"
+    """ hello_hbnb method """
+    return ('Hello HBNB!')
 
 
-@app.route("/hbnb", strict_slashes=False)
-def hbnb():
-    """Displays 'HBNB'."""
-    return "HBNB"
+@app.route('/hbnb', strict_slashes=False)
+def only_hbnb():
+    """ only_hbnb method """
+    return ('HBNB')
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
