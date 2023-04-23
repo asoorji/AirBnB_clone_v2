5c9d67df38324e86473cd222d7ce7716bfd67865
Aso Orji <42071241+asoorji@users.noreply.github.com>
2023-04-23 09:24:56 +0000

    AirBnB v2...

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application:
-    Your web application must be listening on 0.0.0.0, port 5000
-    Routes: 1- /: display “Hello HBNB!”
-            2- /hbnb: display “HBNB”
-            3- /c/<text>: display “C ” followed by the value of the text
-                variable (replace underscore _ symbols with a space )
-    You must use the option strict_slashes=False in your route definition """
+"""Starts a Flask web application
+"""
 from flask import Flask
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ hello_hbnb method """
-    return ('Hello HBNB!')
-
-
-@app.route('/hbnb', strict_slashes=False)
-def only_hbnb():
-    """ only_hbnb method """
-    return ('HBNB')
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def only_c(text):
-    """ only_c method: route to return C followed by text variable, replaces _
-        with spaces """
-    text = text.replace('_', ' ')
-    return ('C' + ' ' + text)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def index():
+        """Display 'Hello HBNB!'
+        """
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """Display 'HBNB'
+        """
+        return 'HBNB'
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """Display “C ” followed by the value of
+        the text variable (replace underscore _
+        symbols with a space)
+        """
+        return 'C ' + text.replace('_', ' ')
+
+    app.run('0.0.0.0')
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,34 +1,88 @@
 #!/usr/bin/python3
-""" this module contains a script that starts a Flask web application
-    the web application must be listening on 0.0.0.0, port 5000
-    Routes: - /states_list """
-from models import *
-from models.base_model import BaseModel, Base
-from models.user import User
-from models.place import Place
+"""Starts a Flask web application
+"""
+from flask import Flask
+from flask import render_template
+from models import storage
 from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from flask import Flask, render_template
-app = Flask(__name__)
-classes = {"Amenity": Amenity, "City": City,
-           "Place": Place, "Review": Review, "State": State, "User": User}
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """ display HTML page with list of cities """
-    states = storage.all(classes["State"]).values()
-    # ^ fetches states data from storage engine, then in line below,
-    # those states are passed into the template
-    return render_template('8-cities_by_states.html', states=states)
-
-
-@app.teardown_appcontext
-def remove_SQLalc_session(exception):
-    """ close storage when tear down is called """
-    storage.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app = Flask(__name__)
+
+    @app.route('/', strict_slashes=False)
+    def index():
+        """Display 'Hello HBNB!'
+        """
+        return 'Hello HBNB!'
+
+    @app.route('/hbnb', strict_slashes=False)
+    def hbnb():
+        """Display 'HBNB'
+        """
+        return 'HBNB'
+
+    @app.route('/c/<text>', strict_slashes=False)
+    def c(text):
+        """Display “C ” followed by the value of
+        the text variable (replace underscore _
+        symbols with a space)
+        """
+        return 'C ' + text.replace('_', ' ')
+
+    @app.route('/python/')
+    @app.route('/python/<text>', strict_slashes=False)
+    def python(text="is cool"):
+        """Display “Python ”, followed by the value of
+        the text variable (replace underscore _
+        symbols with a space )
+        """
+        return 'Python ' + text.replace('_', ' ')
+
+    @app.route('/number/<int:n>', strict_slashes=False)
+    def number(n):
+        """Display “n is a number” only if n is an integer
+        """
+        return str(n) + ' is a number'
+
+    @app.route('/number_template/<int:n>', strict_slashes=False)
+    def number_template(n):
+        """Display a HTML page only if n is an integer
+        """
+        return render_template('5-number.html', n=n)
+
+    @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+    def number_odd_or_even(n):
+        """Display a HTML page only if n is an integer
+        """
+        parity = 'even' if n % 2 == 0 else 'odd'
+        return render_template('6-number_odd_or_even.html', n=n, parity=parity)
+
+    @app.route('/states_list', strict_slashes=False)
+    def states_list():
+        """Display a HTML page of the States
+        """
+        states = storage.all(State).values()
+        return render_template('7-states_list.html', states=states)
+
+    @app.route('/cities_by_states', strict_slashes=False)
+    def cities_by_states():
+        """Display a HTML page of the States and the
+        Cities by State
+        """
+        states = storage.all(State).values()
+        cities = list()
+
+        for state in states:
+            for city in state.cities:
+                cities.append(city)
+
+        return render_template('8-cities_by_states.html',
+                               states=states, state_cities=cities)
+
+    @app.teardown_appcontext
+    def teardown_db(error):
+        """Closes the database again at the end of the request.
+        """
+        storage.close()
+
+    app.run('0.0.0.0')
